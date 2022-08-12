b5048d35808d4eb8c5bda20cdfa367016bfce316
heby <heby@mingyuanyun.com>
2022-08-12 17:14:56 +0800

    feat: 添加根组件html

---
+++ src/Main.tsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import { render } from 'react-dom'
+// import { Provider } from 'react-redux'
+// const ROOT_ELEMENT = document.getElementById('root')
+// render(
+//   <Provider store={}>
+//     <div>hello</div>
+//   </Provider>, ROOT_ELEMENT
+// )
+
