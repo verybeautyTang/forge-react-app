7f01b09ddf2eb2b2525c8c2098f362b352ee6a0a
heby <heby@mingyuanyun.com>
2022-08-22 11:05:31 +0800

    feat: 页面编译显示正常

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,19 +1,24 @@
 
 const { resolve } = require("path")
+
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 const isDev = process.env.NODE_ENV === "development"
 module.exports ={
   entry: resolve(__dirname, "../src/Main.tsx"),
   mode: "development",
+  devtool: "source-map",
   target: 'web',
   output: {
+    publicPath: "/",
     path: resolve(__dirname, "../dist"),
     filename: 'bundle.js'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.tsx', '.ts'],
-    // alias: {
-    //   '@': resolve(__dirname, '../src')
-    // }
+    alias: {
+      '@': resolve(__dirname, '../src')
+    }
   },
   module: {
     rules: [
@@ -24,14 +29,24 @@ module.exports ={
           loader: 'babel-loader',
           options: {
             cacheDirectory: true,
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', "@babel/preset-react"]
           }
         }
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "react-demo",
+      template: resolve(__dirname, "../public") + '/index.html' ,
+      filename: "index.html", 
+      inject: "body",
+    }),
+  ],
   devServer: {
     host: "127.0.0.1",
-    port: 9000
+    port: 9000,
+    open: true,
+    hot: true,
   }
 }
