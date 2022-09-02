bcaa83c5b1ef4d3618071fb63afc57cd35b1701f
heby <heby@mingyuanyun.com>
2022-09-02 16:03:51 +0800

    feat: less文件提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './styles/index.less'
+import styles  from  './index.module.less'
 const App = () => {
   console.log('eee')
   return (
-    <div className="pt10 bg-red"> My name is App2223234</div>
+    <div className={styles['bg-red']}> My name is App2223234</div>
   )
 }
 
